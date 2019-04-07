c0ec250a2cd0d3f36e2037a04d2a148994c6eff0
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2019-04-07 21:59:39 +0200

    updated model

--- src/main/java/com/company/model/Contact.java
+++ src/main/java/com/company/model/Contact.java
@@ -1,14 +1,35 @@
 package com.company.model;
 
 public class Contact {
-    private int id;
-    private Integer idCustomer;
-    private byte type;
-    private String contact;
+    private ContactType contactType;
+    private String contactText;
 
-    public Contact(Integer idCustomer, byte type, String contact) {
-        this.idCustomer = idCustomer;
-        this.type = type;
-        this.contact = contact;
+    public Contact() {
+    }
+
+    public Contact(ContactType contactType, String contactText) {
+        this.contactType = contactType;
+        this.contactText = contactText;
+    }
+
+    public ContactType getContactType() {
+        return contactType;
+    }
+
+    public String getContactText() {
+        return contactText;
+    }
+
+    public enum ContactType {
+        UNKNOWN(0), EMAIL(1), PHONE(2), JABBER(3);
+        private int numericValue;
+
+        ContactType(int numericValue) {
+            this.numericValue = numericValue;
+        }
+
+        public int getNumericValue() {
+            return numericValue;
+        }
     }
 }
--- src/main/java/com/company/model/Customer.java
+++ src/main/java/com/company/model/Customer.java
@@ -1,14 +1,57 @@
 package com.company.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Customer {
-    private int id;
-    private Integer age;
+
+    private String age;
     private String name;
     private String surname;
+    private List<Contact> contacts;
 
-    public Customer(Integer age, String name, String surname) {
+    public Customer() {
+    }
+
+    public Customer(String age, String name, String surname) {
         this.age = age;
         this.name = name;
         this.surname = surname;
+        contacts = null;
+    }
+
+    public void addContact(Contact contact) {
+        if(contacts == null) {
+            contacts = new LinkedList<>();
+        }
+        contacts.add(contact);
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
     }
 }
